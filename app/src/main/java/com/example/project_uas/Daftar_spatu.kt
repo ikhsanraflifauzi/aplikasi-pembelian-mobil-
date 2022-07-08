8b0b0b3f078e128d20166678bf720097890c153a
unknown <ikhsanrafli10@gmail.com>
2022-07-08 09:20:54 +0700

    projct pembuatan aplikasi pembelian mobil berbasis mobile

---
+++ app/src/main/java/com/example/project_uas/Daftar_spatu.kt
@@ -0,0 +1,11 @@
+package com.example.project_uas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Daftar_spatu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daftar_spatu)
+    }
+}
---
+++ app/src/main/java/com/example/project_uas/Deskripsi_Pembelian.kt
@@ -0,0 +1,11 @@
+package com.example.project_uas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Deskripsi_Pembelian : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deskripsi_pembelian)
+    }
+}
